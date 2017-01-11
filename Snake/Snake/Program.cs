ecdc9c03a05d9c07c52a1e141de22d94b0369a24
xelsinki <xelsinki@hotmail.com>
2017-01-11 23:18:07 +0200

    Lesson 3
    Point

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -9,7 +9,30 @@ namespace Snake
     class Program
     {
         static void Main(string[] args)
-        {
+       
+{
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 11;
+            p2.y = 13;
+            p2.sym = '*';
+            p2.Draw();
+
+            /*  int x1 = 1;
+                int y1 = 3;
+                char sym1 = '*';
+                Draw (x1, y1, sym1);  */
+
+            Console.ReadLine();
+                }
+
+              }
+
         }
-    }
-}
+
+ 
